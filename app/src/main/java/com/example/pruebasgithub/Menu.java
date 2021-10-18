84447e6e1e1df62498792929820824d388855294
Diana <dzelten@gmail.com>
2021-10-17 23:42:18 -0500

    Boton

--- app/src/main/java/com/example/pruebasgithub/Menu.java
+++ app/src/main/java/com/example/pruebasgithub/Menu.java
@@ -1,4 +1,14 @@
 package com.example.pruebasgithub;
 
-public class Menu {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
