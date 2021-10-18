8f8b730727d82c6c667b1ea52a06c05ef69c79ee
mcdiazgbravo <52355710+mcdiazgbravo@users.noreply.github.com>
2021-10-18 00:03:27 -0500

    Boton arreglado

--- app/src/main/java/com/example/pruebasgithub/Menu.java
+++ app/src/main/java/com/example/pruebasgithub/Menu.java
@@ -7,8 +7,9 @@ import android.os.Bundle;
 public class Menu extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_dos);
     }
 }
